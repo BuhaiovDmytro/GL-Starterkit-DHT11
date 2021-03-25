513eca995fd1865dd0f3b02773814956c4adb041
Dmytro Buhaiov <d.bugauov@gmail.com>
2021-03-25 11:57:42 +0200

    Add photo, removed comments

--- Core/Inc/DHT.h
+++ Core/Inc/DHT.h
@@ -3,24 +3,22 @@
 
 #include "main.h"
 
-/* Структура возвращаемых датчиком данных */
 typedef struct {
 	float hum;
 	float temp;
 } DHT_data;
-/* Тип используемого датчика */
+
 typedef enum {
 	DHT11,
 	DHT22
 } DHT_type;
 
-/* Настройки */
-#define DHT_Port 	GPIOD				//Группа линии данных
-#define DHT_Pin 	GPIO_PIN_11 	//Пин линии данных
-#define DHT_PullUp 0					//Нужно ли включать внутреннюю подтяжку пина к питанию
-#define DHT_timeout 10000			//Количество итераций, после которых функция вернёт пустые значения
+#define DHT_Port 	GPIOD
+#define DHT_Pin 	GPIO_PIN_11
+#define DHT_PullUp 0
+#define DHT_timeout 10000
 
-/* Прототипы функций */
-DHT_data DHT_getData(DHT_type t); //Получить данные с датчика
+
+DHT_data DHT_getData(DHT_type t);
 
 #endif
--- README.md
+++ README.md
@@ -4,3 +4,8 @@ STM32CubeIDE project for GL Starterkit (based on STM32f407 Discovery). Includes 
 The program reads data from DHT11 and outputs it to the screen.
 
 The library for DHT taken from https://github.com/RainbowKiwiFOX/DHT
+
+###Photo of the working program:
+
+![alt text](https://github.com/BuhaiovDmytro/GL-Starterkit-DHT11/blob/master/DHT11_Working.PNG?raw=true)
+
